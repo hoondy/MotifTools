bad3e4de8146c29538ff1e02c1f9bd31ff2a73d5
Donghoon Lee <dhrlee@gmail.com>
2016-09-30 17:16:06 -0400

    display variant position wrt motif

--- procMotif.py
+++ procMotif.py
@@ -9,7 +9,8 @@ __maintainer__ = "Donghoon Lee"
 __email__ = "donghoon.lee@yale.edu"
 
 from Bio import motifs, SeqIO
-from Bio.Alphabet import IUPAC
+from Bio.Seq import Seq
+from Bio.Alphabet import IUPAC, generic_dna
 import math, collections, ConfigParser, glob, re
 import numpy as np
 from os import listdir
@@ -32,26 +33,48 @@ C_PRECISION = int(Config.get("param","C_PRECISION"))
 
 ###
 
-def get_jaspar_motif(motifName):
+def listOnlyFiles(path):
+    return [f for f in listdir(path) if isfile(join(path, f))]
+
+def ls(path):
+    return glob.glob(path)
+
+def grep(pattern, file):
+    output = []
+    with open(file) as f:
+        for line in f.readlines():
+            if re.search(pattern, line, re.IGNORECASE):
+                output.append(line.rstrip())
+    return output
+
+def findMatchedPeak(cell, rawTF):
+    for filepath in ls(Config.get("data","peak_bed")+'/*.bed'):
+        filename = filepath.rstrip().split("/")[-1]
+        filename_split = filename.rstrip().split("_")
+        if filename_split[4] == cell and filename_split[5] == rawTF:
+            print "Found",filename,"for TF:",parseTF(filename_split[5])
+
+###
+
+def get_jaspar_motif(tfName):
     with open(Config.get("data","pfm_db_jaspar")) as handle:
         for m in motifs.parse(handle, "jaspar"):
-            if str(m.name).upper() == str(motifName).upper():
-                print motifName,"matches with JASPAR MOTIF ID",m.matrix_id
+            if str(m.name).upper() == str(tfName).upper():
                 return m
-
-    print "WARNING: JASPAR motif NOT found!"
+    # if not found
     return None
 
-def parseJaspar(motifName):
+def parseJaspar(tfName):
+    m = get_jaspar_motif(tfName)
+    if m == None:
+        return None # stop if not found
+    writePFM(m)
+    writePPM(m,C_PSEUDOCOUNTS)
+    writePWM(m,C_PSEUDOCOUNTS,C_BACKGROUND)
+    return m
 
-    with open(Config.get("data","pfm_db_jaspar")) as handle:
-        for m in motifs.parse(handle, "jaspar"):
-            if str(m.name).upper() == str(motifName).upper():
-                writePFM(m)
-                writePPM(m,C_PSEUDOCOUNTS)
-                writePWM(m,C_PSEUDOCOUNTS,C_BACKGROUND)
-                return m ### stop if motif is found
-    return None
+def parseTF(rawName):
+    return rawName.rstrip().split("-")[-1]
 
 def writePFM(m):
     with open("MOTIF_"+str(m.name).upper()+".pfm", "w") as output:
@@ -77,23 +100,11 @@ def writePWM(m):
                 output.write(str(round(x,3))+" ")
             output.write("\n")
 
-def dscoreAnalysis(motifName, refFasta, altFasta, pvalThreshold, outFile):
+def pwm2scaled_pwm(m, pwm):
 
-    with open(Config.get("data","pfm_db_jaspar")) as handle:
-        for m in motifs.parse(handle, "jaspar"):
-            if str(m.name).upper() == str(motifName).upper():
-                ppm = m.counts.normalize(pseudocounts=C_PSEUDOCOUNTS)
-                pwm = ppm.log_odds(background=C_BACKGROUND)
-                # writePWM(m)
-                break ### stop if motif is found
-    print "PFM:"
-    print m.counts
-    print "PPM:"
-    print ppm
-    print "PWM:"
-    print pwm
-
+    ###
     ### scale raw PWM scores ###
+    ###
 
     # copy to np matrix
     scaled_pwm = np.zeros(shape=(4,len(m)))
@@ -111,94 +122,29 @@ def dscoreAnalysis(motifName, refFasta, altFasta, pvalThreshold, outFile):
     # round to nearest integer
     scaled_pwm = np.rint(scaled_pwm).astype(int)
 
-    print "Scaled PWM:"
-    print scaled_pwm
+    return scaled_pwm
 
+def scaled_pwm2scoredist(m, scaled_pwm):
+
+    ###
     ### score distribution
-    score_distribution = np.zeros(shape=(len(m),len(m)*C_PRECISION), dtype=np.int)
-    # print np.shape(score_distribution)
+    ###
 
+    score_distribution = np.zeros(shape=(len(m),len(m)*C_PRECISION), dtype=np.int)
+    print np.shape(score_distribution)
+
+    ### proc tf motif
     # init first row
     for i, nt in enumerate(['A','C','G','T']):
         score_distribution[0,scaled_pwm[i,0]] += 1
-
-    # proc rest of motif
+    # rest of motif
     for j in range(1,len(m)): ### j: 1 to length of motif
         # print "MOTIF pos:", j
         for k in scaled_pwm[:,j]:
             for idx, count in enumerate(score_distribution[j-1,:]):
                 if count > 0:
                     score_distribution[j,idx+k] += count
-
-    ### score threshold: discard motif with score smaller than this
-    score_threshold = pval2score(pvalThreshold, score_distribution)
-
-    ### LOAD FASTA SEQ
-    ref_seq = collections.OrderedDict()
-    alt_seq = collections.OrderedDict()
-
-    for r1 in SeqIO.parse(refFasta,"fasta",alphabet=IUPAC.unambiguous_dna):
-        ref_seq[r1.id]=r1.seq
-    print "Processing",len(ref_seq),"peaks from REF genome"
-
-    for r2 in SeqIO.parse(altFasta,"fasta",alphabet=IUPAC.unambiguous_dna):
-        alt_seq[r2.id]=r2.seq
-    print "Processing",len(alt_seq),"peaks from ALT genome"
-
-    with open(outFile, 'w') as output:
-        for skey in ref_seq.keys():
-
-            ### SLIDE WINDOW
-            for pos in range(0,len(ref_seq[skey])-len(m)+1):
-
-                chr = skey.split(":")[0]
-                pos_start = int(skey.split(":")[1].split("-")[0]) + pos + 1 ### convert 0-based to 1-based
-                pos_end = int(skey.split(":")[1].split("-")[0]) + pos + len(m)
-                ucsc_coord = chr+":"+str(pos_start)+"-"+str(pos_end)
-
-                ref_subseq_pos = ref_seq[skey][pos:pos+len(m)]
-                alt_subseq_pos = alt_seq[skey][pos:pos+len(m)]
-
-                ref_subseq_neg = ref_seq[skey][pos:pos+len(m)].reverse_complement()
-                alt_subseq_neg = alt_seq[skey][pos:pos+len(m)].reverse_complement()
-
-                ### SKIP EVALUATING SUB-SEQ if ref and alt is the same
-                if ref_subseq_pos != alt_subseq_pos:
-
-                    ### calc motif score ###
-
-                    ### FORWARD STRAND ###
-
-                    ref_score_pos = int(seq2score(ref_subseq_pos,scaled_pwm))
-                    alt_score_pos = int(seq2score(alt_subseq_pos,scaled_pwm))
-
-                    if ref_score_pos > score_threshold or alt_score_pos > score_threshold:
-                        ref_pval_pos = score2pval(ref_score_pos, score_distribution)
-                        alt_pval_pos = score2pval(alt_score_pos, score_distribution)
-
-                        dscore_pos = -10 * math.log10(ref_pval_pos/alt_pval_pos)
-                        ref_rawscore_pos = seq2score(ref_subseq_pos,pwm)
-                        alt_rawscore_pos = seq2score(alt_subseq_pos,pwm)
-                        if abs(dscore_pos) > 0:
-                            # print dscore_pos
-                            output.write(chr+"\t"+str(pos_start-1)+"\t"+str(pos_end)+"\t"+ucsc_coord+"_+\t"+str(dscore_pos)+"\t+\t"+str(ref_pval_pos)+"\t"+str(alt_pval_pos)+"\t"+str(ref_subseq_pos)+"\t"+str(alt_subseq_pos)+"\t"+str(ref_rawscore_pos)+"\t"+str(alt_rawscore_pos)+"\n")
-
-                    ### REVERSE STRAND ###
-
-                    ref_score_neg = int(seq2score(ref_subseq_neg,scaled_pwm))
-                    alt_score_neg = int(seq2score(alt_subseq_neg,scaled_pwm))
-
-                    if ref_score_neg > score_threshold or alt_score_neg > score_threshold:
-                        ref_pval_neg = score2pval(ref_score_neg, score_distribution)
-                        alt_pval_neg = score2pval(alt_score_neg, score_distribution)
-
-                        dscore_neg = -10 * math.log10(ref_pval_neg/alt_pval_neg)
-                        ref_rawscore_neg = seq2score(ref_subseq_neg,pwm)
-                        alt_rawscore_neg = seq2score(alt_subseq_neg,pwm)
-                        if abs(dscore_neg) > 0:
-                            # print dscore_neg
-                            output.write(chr+"\t"+str(pos_start)+"\t"+str(pos_end+1)+"\t"+ucsc_coord+"_-\t"+str(dscore_neg)+"\t-\t"+str(ref_pval_neg)+"\t"+str(alt_pval_neg)+"\t"+str(ref_subseq_neg)+"\t"+str(alt_subseq_neg)+"\t"+str(ref_rawscore_neg)+"\t"+str(alt_rawscore_neg)+"\n")
-        # break
+    return score_distribution
 
 def pwm2pval(motifName, seq):
 
@@ -274,26 +220,247 @@ def pval2score(pval, score_distribution):
             # print float(cumsum) / float(math.pow(4,len(score_distribution)))
             return idx
 
-def listOnlyFiles(path):
-    return [f for f in listdir(path) if isfile(join(path, f))]
+def dscoreAnalysis(tfName, fastaFile, bedFile, pvalThreshold, outFile):
 
-def ls(path):
-    return glob.glob(path)
+    ##############################
+    ### 1. PROCESS TF MOTIF
+    ##############################
 
-def grep(pattern, file):
-    output = []
-    with open(file) as f:
-        for line in f.readlines():
-            if re.search(pattern, line, re.IGNORECASE):
-                output.append(line.rstrip())
-    return output
+    ### get JASPAR TF motif
+    m = get_jaspar_motif(tfName)
 
-def findMatchedPeak(cell, rawTF):
-    for filepath in ls(Config.get("data","peak_bed")+'/*.bed'):
-        filename = filepath.rstrip().split("/")[-1]
-        filename_split = filename.rstrip().split("_")
-        if filename_split[4] == cell and filename_split[5] == rawTF:
-            print "Found",filename,"for TF:",parseTF(filename_split[5])
+    # stop if not found
+    if not m:
+        return None
 
-def parseTF(rawName):
-    return rawName.rstrip().split("-")[-1]
+    ppm = m.counts.normalize(pseudocounts=C_PSEUDOCOUNTS)
+    pwm = ppm.log_odds(background=C_BACKGROUND)
+
+    print "PFM:"
+    print m.counts
+    print "PPM:"
+    print ppm
+    print "PWM:"
+    print pwm
+
+    scaled_pwm = pwm2scaled_pwm(m, pwm)
+
+    print "Scaled PWM:"
+    print scaled_pwm
+
+    score_distribution = scaled_pwm2scoredist(m, scaled_pwm)
+
+    ### score threshold: discard motif with score smaller than this
+    score_threshold = pval2score(pvalThreshold, score_distribution)
+
+    ##############################
+    ### 2. PROCESS FASTA SEQ
+    ##############################
+
+    ### LOAD FASTA SEQ
+    peak_seq = collections.OrderedDict()
+
+    for seq_item in SeqIO.parse(fastaFile,"fasta",alphabet=generic_dna):
+        peak_seq[seq_item.id]=seq_item.seq
+        # print seq_item.id
+
+    print "Processing",len(peak_seq),"peaks from REF genome"
+
+    ##############################
+    ### 3. PROCESS BED
+    ##############################
+
+    with open(outFile, 'w') as output:
+        with open(bedFile, 'r') as input:
+            for line in input.readlines():
+                line_split = line.rstrip().split("\t")
+                # print line_split
+
+                # sequence info
+                seq_chr = line_split[0]
+                seq_start = int(line_split[1])
+                seq_end = int(line_split[2])
+                skey = seq_chr+":"+str(seq_start)+"-"+str(seq_end) # 0-based
+                # print skey
+                seq = peak_seq[skey]
+                # print seq
+
+                # variant info
+                var_chr = line_split[10]
+                var_pos = int(line_split[11])-1 # 1-based pos to 0-based pos
+                var_ref = line_split[13]
+                var_alt = line_split[14]
+                # print var_chr,var_pos,var_ref,var_alt
+
+                # seq before variant
+                seq_prefix = seq[:var_pos-seq_start]
+                # seq after variant
+                seq_postfix = seq[var_pos+1-seq_start:]
+
+                left = max(var_pos+1-len(m),var_pos+1-len(seq_prefix))
+                right = min(var_pos+len(m),var_pos+len(seq_postfix))
+                # print left,right
+
+                for subseq_start in range(left,right+1-len(m)):
+
+                    ucsc_coord = seq_chr+":"+str(subseq_start+1)+"-"+str(subseq_start+len(m)) # 0-based to 1-based
+                    # print ucsc_coord
+
+                    # var pos wrt to motif
+                    motif_varpos_pos = var_pos-subseq_start+1 # 1-based variant position wrt motif
+                    motif_varpos_neg = len(m)-var_pos+subseq_start # 1-based variant position wrt motif
+                    # print "var pos wrt TF motif (+):",motif_varpos_pos
+                    # print "var pos wrt TF motif (-):",motif_varpos_neg
+
+                    subseq_prefix = seq[subseq_start-seq_start:var_pos-seq_start]
+                    subseq_var = seq[var_pos-seq_start]
+                    subseq_postfix = seq[var_pos-seq_start+1:subseq_start-seq_start+len(m)]
+
+                    if subseq_var.upper() != var_ref.upper():
+                        print "WARNING! Reference sequence mismatch:",subseq_var,var_ref,var_pos
+                        return None
+
+                    subseq_ref_pos = subseq_prefix+var_ref.upper()+subseq_postfix
+                    subseq_alt_pos = subseq_prefix+var_alt.upper()+subseq_postfix
+
+                    subseq_ref_pos_print = subseq_prefix+"["+var_ref.upper()+"]"+subseq_postfix
+                    subseq_alt_pos_print = subseq_prefix+"["+var_alt.upper()+"]"+subseq_postfix
+
+                    # print subseq_ref_pos, subseq_alt_pos
+                    # print subseq_ref_pos_print, subseq_alt_pos_print
+
+                    subseq_ref_neg = subseq_ref_pos.reverse_complement()
+                    subseq_alt_neg = subseq_alt_pos.reverse_complement()
+
+                    subseq_ref_neg_print = subseq_postfix.reverse_complement()+"["+Seq(var_ref.upper(),generic_dna).reverse_complement()+"]"+subseq_prefix.reverse_complement()
+                    subseq_alt_neg_print = subseq_postfix.reverse_complement()+"["+Seq(var_alt.upper(),generic_dna).reverse_complement()+"]"+subseq_prefix.reverse_complement()
+
+                    # print subseq_ref_neg, subseq_alt_neg
+                    # print subseq_ref_neg_print, subseq_alt_neg_print
+
+                    ### calc motif score ###
+
+                    ### FORWARD STRAND ###
+
+                    ref_score_pos = int(seq2score(subseq_ref_pos,scaled_pwm))
+                    alt_score_pos = int(seq2score(subseq_alt_pos,scaled_pwm))
+
+                    if ref_score_pos > score_threshold or alt_score_pos > score_threshold:
+                        ref_pval_pos = score2pval(ref_score_pos, score_distribution)
+                        alt_pval_pos = score2pval(alt_score_pos, score_distribution)
+
+                        dscore_pos = -10 * math.log10(ref_pval_pos/alt_pval_pos)
+                        ref_rawscore_pos = seq2score(subseq_ref_pos,pwm)
+                        alt_rawscore_pos = seq2score(subseq_alt_pos,pwm)
+                        if abs(dscore_pos) > 0:
+                            # print dscore_pos
+                            output.write(seq_chr+"\t"+str(subseq_start)+"\t"+str(subseq_start+len(m))+"\t"+ucsc_coord+"_+\t"+str(dscore_pos)+"\t+\t"+str(ref_pval_pos)+"\t"+str(alt_pval_pos)+"\t"+str(subseq_ref_pos_print)+"\t"+str(subseq_alt_pos_print)+"\t"+str(ref_rawscore_pos)+"\t"+str(alt_rawscore_pos)+"\t"+str(motif_varpos_pos)+"\n")
+
+                    ### REVERSE STRAND ###
+
+                    ref_score_neg = int(seq2score(subseq_ref_neg,scaled_pwm))
+                    alt_score_neg = int(seq2score(subseq_alt_neg,scaled_pwm))
+
+                    if ref_score_neg > score_threshold or alt_score_neg > score_threshold:
+                        ref_pval_neg = score2pval(ref_score_neg, score_distribution)
+                        alt_pval_neg = score2pval(alt_score_neg, score_distribution)
+
+                        dscore_neg = -10 * math.log10(ref_pval_neg/alt_pval_neg)
+                        ref_rawscore_neg = seq2score(subseq_ref_neg,pwm)
+                        alt_rawscore_neg = seq2score(subseq_alt_neg,pwm)
+                        if abs(dscore_neg) > 0:
+                            # print dscore_neg
+                            output.write(seq_chr+"\t"+str(subseq_start)+"\t"+str(subseq_start+len(m)+1)+"\t"+ucsc_coord+"_-\t"+str(dscore_neg)+"\t-\t"+str(ref_pval_neg)+"\t"+str(alt_pval_neg)+"\t"+str(subseq_ref_neg_print)+"\t"+str(subseq_alt_neg_print)+"\t"+str(ref_rawscore_neg)+"\t"+str(alt_rawscore_neg)+"\t"+str(motif_varpos_neg)+"\n")
+
+def bscoreAnalysis(tfName, fastaFile, pvalThreshold, outFile):
+
+    ##############################
+    ### 1. PROCESS TF MOTIF
+    ##############################
+
+    ### get JASPAR TF motif
+    m = get_jaspar_motif(tfName)
+
+    # stop if not found
+    if not m:
+        return None
+
+    ppm = m.counts.normalize(pseudocounts=C_PSEUDOCOUNTS)
+    pwm = ppm.log_odds(background=C_BACKGROUND)
+
+    print "PFM:"
+    print m.counts
+    print "PPM:"
+    print ppm
+    print "PWM:"
+    print pwm
+
+    scaled_pwm = pwm2scaled_pwm(m, pwm)
+
+    print "Scaled PWM:"
+    print scaled_pwm
+
+    score_distribution = scaled_pwm2scoredist(m, scaled_pwm)
+
+    ### score threshold: discard motif with score smaller than this
+    score_threshold = pval2score(pvalThreshold, score_distribution)
+
+    ##############################
+    ### 2. PROCESS FASTA SEQ
+    ##############################
+
+    ### LOAD FASTA SEQ
+    peak_seq = collections.OrderedDict()
+
+    for seq_item in SeqIO.parse(fastaFile,"fasta",alphabet=generic_dna):
+        peak_seq[seq_item.id]=seq_item.seq
+        # print seq_item.id
+
+    print "Processing",len(peak_seq),"peaks from REF genome"
+
+    # with open(outFile, 'w') as output:
+    #     for skey in ref_seq.keys():
+    #
+    #         ### SLIDE WINDOW
+    #         for pos in range(0,len(ref_seq[skey])-len(m)+1):
+    #
+    #             chr = skey.split(":")[0]
+    #             pos_start = int(skey.split(":")[1].split("-")[0]) + pos + 1 ### convert 0-based to 1-based
+    #             pos_end = int(skey.split(":")[1].split("-")[0]) + pos + len(m)
+    #             ucsc_coord = chr+":"+str(pos_start)+"-"+str(pos_end)
+    #
+    #             ref_subseq_pos = ref_seq[skey][pos:pos+len(m)]
+    #             ref_subseq_neg = ref_seq[skey][pos:pos+len(m)].reverse_complement()
+    #
+    #
+    #             ### FORWARD STRAND ###
+    #
+    #             ref_score_pos = int(seq2score(ref_subseq_pos,scaled_pwm))
+    #
+    #             if ref_score_pos > score_threshold:
+    #                 ref_pval_pos = score2pval(ref_score_pos, score_distribution)
+    #
+    #                 # dscore_pos = -10 * math.log10(ref_pval_pos/alt_pval_pos)
+    #                 ref_rawscore_pos = seq2score(ref_subseq_pos,pwm)
+    #
+    #                 print ref_subseq_pos, ref_pval_pos
+    #                 # if abs(dscore_pos) > 0:
+    #                 #     # print dscore_pos
+    #                 #     output.write(chr+"\t"+str(pos_start-1)+"\t"+str(pos_end)+"\t"+ucsc_coord+"_+\t"+str(dscore_pos)+"\t+\t"+str(ref_pval_pos)+"\t"+str(alt_pval_pos)+"\t"+str(ref_subseq_pos)+"\t"+str(alt_subseq_pos)+"\t"+str(ref_rawscore_pos)+"\t"+str(alt_rawscore_pos)+"\n")
+    #
+    #             ### REVERSE STRAND ###
+    #
+    #             ref_score_neg = int(seq2score(ref_subseq_neg,scaled_pwm))
+    #             # alt_score_neg = int(seq2score(alt_subseq_neg,scaled_pwm))
+    #
+    #             if ref_score_neg > score_threshold:
+    #                 ref_pval_neg = score2pval(ref_score_neg, score_distribution)
+    #                 # alt_pval_neg = score2pval(alt_score_neg, score_distribution)
+    #
+    #                 # dscore_neg = -10 * math.log10(ref_pval_neg/alt_pval_neg)
+    #                 ref_rawscore_neg = seq2score(ref_subseq_neg,pwm)
+    #                 # alt_rawscore_neg = seq2score(alt_subseq_neg,pwm)
+    #                 # if abs(dscore_neg) > 0:
+    #                 #     # print dscore_neg
+    #                 #     output.write(chr+"\t"+str(pos_start)+"\t"+str(pos_end+1)+"\t"+ucsc_coord+"_-\t"+str(dscore_neg)+"\t-\t"+str(ref_pval_neg)+"\t"+str(alt_pval_neg)+"\t"+str(ref_subseq_neg)+"\t"+str(alt_subseq_neg)+"\t"+str(ref_rawscore_neg)+"\t"+str(alt_rawscore_neg)+"\n")
+    # # break
--- runDscore.py
+++ runDscore.py
@@ -18,47 +18,53 @@ Config.read("config.ini")
 ### LOAD ARGs ###
 
 parser = argparse.ArgumentParser(description='Calculate TFBS D-score')
-parser.add_argument('-b','--bed', help='Input Peak BED File',required=True)
-parser.add_argument('-v','--vcf', help='Input Variant VCF File',required=True)
-parser.add_argument('-r','--ref', help='REF Genome',required=True)
-parser.add_argument('-a','--alt', help='ALT Genome',required=True)
+parser.add_argument('-s','--sample', help='Sample Name',required=True)
+parser.add_argument('-t','--tf', help='TF Name',required=True)
+parser.add_argument('-b','--bed', help='Peak BED File',required=True)
+parser.add_argument('-v','--vcf', help='Variant VCF File',required=True)
+parser.add_argument('-r','--ref', help='REF Genome FASTA File',required=True)
 args = parser.parse_args()
 
 ###
 
 def getPeakWithVariant(bedFile, vcfFile, outFile):
-    subprocess.call(Config.get("app","bedtools")+" intersect -a "+bedFile+" -b "+vcfFile+" -u > "+outFile, shell=True)
+    subprocess.call(Config.get("app","bedtools")+" intersect -a "+bedFile+" -b "+vcfFile+" -wa -wb > "+outFile, shell=True)
     return outFile
 
 def getFasta(fastaFile, bedFile, outFile):
     subprocess.call(Config.get("app","bedtools")+" getfasta -fi "+fastaFile+" -bed "+bedFile+" -fo "+outFile, shell=True)
     return outFile
 
-def main(bedFile):
-    filename = bedFile.rstrip().split("/")[-1]
-    filename_split = filename.rstrip().split("_")
+def main():
 
-    cell = filename_split[4]
-    tf = procMotif.parseTF(filename_split[5])
+    sample = args.sample
+    tf = procMotif.parseTF(args.tf)
 
-    print "Processing",cell,"TF:",tf
+    print "Sample:",sample
+    print "TF:",tf
 
-    if procMotif.get_jaspar_motif(tf):
+    m = procMotif.get_jaspar_motif(tf)
 
-        # intersect peak with variants
-        print "Intersect peak with variants"
-        getPeakWithVariant(bedFile, args.vcf, "tf_peak_"+cell+"_"+tf+".bed")
-        print "DONE"
+    # stop if not found
+    if not m:
+        print "WARNING: JASPAR motif NOT found"
+        return None
 
-        # getfasta
-        print "Get FASTA"
-        getFasta(args.ref, "tf_peak_"+cell+"_"+tf+".bed", "tf_peak_"+cell+"_"+tf+"_ref.fa")
-        getFasta(args.alt, "tf_peak_"+cell+"_"+tf+".bed", "tf_peak_"+cell+"_"+tf+"_alt.fa")
-        print "DONE"
+    print "JASPAR motif ID:",m.matrix_id
 
-        # calculate D-score
-        print "Calculate D-score"
-        procMotif.dscoreAnalysis(tf, "tf_peak_"+cell+"_"+tf+"_ref.fa", "tf_peak_"+cell+"_"+tf+"_alt.fa", float(Config.get("param","C_PVAL_THRESHOLD")), "dscore_"+cell+"_"+tf+".bed")
-        print "DONE"
+    # intersect peak with variants
+    print "Intersect peak with variants"
+    getPeakWithVariant(args.bed, args.vcf, "tf_peak_"+sample+"_"+tf+".bed")
+    print "DONE"
 
-main(args.bed)
+    # getfasta
+    print "Get FASTA"
+    getFasta(args.ref, "tf_peak_"+sample+"_"+tf+".bed", "tf_peak_"+sample+"_"+tf+".fa")
+    print "DONE"
+
+    # calculate D-score
+    print "Calculate D-score"
+    procMotif.dscoreAnalysis(tf, "tf_peak_"+sample+"_"+tf+".fa", "tf_peak_"+sample+"_"+tf+".bed", float(Config.get("param","C_PVAL_THRESHOLD")), "dscore_"+sample+"_"+tf+".bed")
+    print "DONE"
+
+main()
